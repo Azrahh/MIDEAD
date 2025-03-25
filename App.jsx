3d92dbe1d77bb4a5d4227d0e66d6443004825335
Azrah Jawaid <Azrajawaid62@gmail.com>
2025-03-25 09:53:26 +0500

    Heading Made

--- App.jsx
+++ App.jsx
@@ -6,5 +6,14 @@ function app ()
     const [counter, setCounter]=UseState(0);
     const incrementCounter=() => {
         setCounter(counter+1);
-    }
+    };
+    return(
+    <div className="MY APP">
+       <h1>Welcome to react</h1>
+       <p>Current Counter: {counter}</p>
+       <button onClick={incrementCount}>Increment Counter</button>
+       
+
+    </div>
+    );
 }
