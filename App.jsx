720f38c824b42339b633926e7f877b1cdbc52d69
Azrah Jawaid <Azrajawaid62@gmail.com>
2025-03-25 10:11:48 +0500

    Again Updated

--- App.jsx
+++ App.jsx
@@ -1,4 +1,8 @@
 import React , {UseState} from "react";
+import CountdownTimer from './CountdownTimer';
+import LightSwitch from './LightSwitch'; 
+import ReactDOM from 'react-dom';
+import './index.css';
 import './App.css';
 
 function app ()
@@ -9,10 +13,19 @@ function app ()
     };
     return(
     <div className="MY APP">
-       <h1>Welcome to react</h1>
-       <p>Current Counter: {counter}</p>
+       <h1>React Count Down Timer And Light Switch</h1>
+       <div className="App container">
+       <div className="CountdownTimer">
+        <h2>Countdown Timer</h2>
+        <p>{timeLeft} seconds left</p>
+        <button onClick={() => setIsTimerRunning(true)} disabled={isTimerRunning}>Start</button>
+        <button onClick={() => setIsTimerRunning(false)} disabled={!isTimerRunning}>Stop</button>
+      </div>
+      
+        <CountdownTimer initialTime={10}/>
+       <LightSwitch/>
        <button onClick={incrementCount}>Increment Counter</button>
-       
+       </div>
 
     </div>
     );
